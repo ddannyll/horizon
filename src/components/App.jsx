bdcad665ceba7de33580dcbe63164ef796573f83
Daniel Nguyen <daniel.nguyen.s173@gmail.com>
2023-03-19 03:23:49 +1100

    move quotes to config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react'
 import BlackHole from './BlackHole'
 import Stars from './Stars'
 import Typed from 'typed.js'
+import { EXTRA_QUOTES, QUOTES } from '../config'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,21 +10,7 @@ function App() {
 
   useEffect(() => {
     const typed = new Typed(typedElement.current, {
-      strings: [
-        "Mankind Was Born On Earth. It Was Never Meant To Die Here.",
-        "Murphy's Law Doesn’t Mean That Something Bad Will Happen...", 
-        "...It Means That Whatever Can Happen Will Happen.",
-        "Every hour we spend on that planet will be 7 years back on Earth.",
-        "A machine doesn't improvise well because you cannot program a fear of death.",
-        "Our survival instinct is our greatest source of inspiration.",
-        "No time for caution.",
-        "We’ve Always Defined Ourselves By The Ability To Overcome The Impossible.",
-        "Do not go gentle into that good night...",
-        "...Old age should burn and rave at close of day...",
-        "...Rage, rage against the dying of the light.",
-        "We Must Reach Far Beyond Our Own Lifespans.",
-        "Interstellar.", 
-      ],
+      strings: QUOTES,
       typeSpeed: 50,
       backSpeed: 200,
       backDelay: 3000,
@@ -37,7 +24,7 @@ function App() {
 
   return (
     <div className="App w-screen h-screen bg-zinc-900 absolute top-0 left-0">
-      <div className="p-10 h-1/3 inline mx-auto flex text-center justify-center text-white font-mono text-2xl relative z-10 bg-gradient-to-b from-[#18181Bee]">
+      <div className="p-10 h-1/3 inline mx-auto flex text-center justify-center text-white font-mono text-2xl relative z-10 bg-gradient-to-b from-zinc-900">
         <div className="container">
           <span ref={typedElement}/>
         </div>
